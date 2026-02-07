61b1bfaf56c5371edb7945864675ed0637bedf48
Riegooo <christiandanielcagas0@gmail.com>
2026-02-07 09:43:30 +0800

    declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world

---
+++ Java_file/day2.java
@@ -0,0 +1,11 @@
+package coding_10min_a_day.Java_file;
+public class day2 {
+    public static void main(String[] args) {
+        
+        String first = "Hello";
+        String second = "World";
+        
+        System.out.println(first + " " + second);
+
+    }
+}
