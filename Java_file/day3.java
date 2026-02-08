ccd64f8850ab23feba7e5acde28a23edfc0168ce
Riegooo <christiandanielcagas0@gmail.com>
2026-02-08 11:22:45 +0800

    Trim whitespace from strings using built in function

---
+++ Java_file/day3.java
@@ -0,0 +1,11 @@
+package coding_10min_a_day.Java_file;
+
+public class day3 {
+    public static void main(String[] args) {
+
+        String name = "         Java";
+        System.out.println(name);
+        System.out.println(name.trim());
+
+    }
+}
