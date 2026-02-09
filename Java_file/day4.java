d76e6eb6b1d96aed479a32076a43a6184f3101e8
Riegooo <christiandanielcagas0@gmail.com>
2026-02-09 10:21:36 +0800

    Day4 - uppercase-all and lowercase-all strings using built in function

---
+++ Java_file/day4.java
@@ -0,0 +1,13 @@
+package coding_10min_a_day.Java_file;
+
+public class day4 {
+    public static void main(String[] args) {
+        
+        String first = "hello";
+        String second = "WORLD";
+
+        System.out.println(first.toUpperCase());
+        System.out.println(second.toLowerCase());
+
+    }
+}
