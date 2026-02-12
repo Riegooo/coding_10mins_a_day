50ba98a68882b0be5e15b93377707400b7e67e7a
Riegooo <christiandanielcagas0@gmail.com>
2026-02-12 11:45:19 +0800

    Day 6 - concatenate integer and string without getting an error

---
+++ Java_file/day6.java
@@ -0,0 +1,12 @@
+package coding_10min_a_day.Java_file;
+
+public class day6 {
+    public static void main(String[] args) {
+
+        String year = "Year: ";
+        int num = 2026;
+
+        System.out.println(year + num);
+
+    }
+}
