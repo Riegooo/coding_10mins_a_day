c394ed702ed8b93507e027dc1a98fdab631ae581
Riegooo <christiandanielcagas0@gmail.com>
2026-02-14 04:18:51 +0800

    Day 8 - contatenate boolean and string without getting an error

---
+++ Java_file/day8.java
@@ -0,0 +1,12 @@
+package coding_10min_a_day.Java_file;
+
+public class day8 {
+    public static void main(String[] args) {
+        
+        String text = "Is active: ";
+        boolean status = true;
+        
+        System.out.println(text + status);
+
+    }
+}
