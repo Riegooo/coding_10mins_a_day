564d0cd2987cb58dd0392592878005488479367c
Riegooo <christiandanielcagas0@gmail.com>
2026-02-13 10:46:13 +0800

    Day 7 - concatenate float and string without getting an error

---
+++ Java_file/day7.java
@@ -0,0 +1,14 @@
+package coding_10min_a_day.Java_file;
+
+public class day7 {
+    public static void main(String[] args) {
+        
+        String text = "0.539 / ";
+        float Float = 0.11f;
+
+        String divde = text + Float;
+
+        System.out.println(divde);
+
+    }
+}
