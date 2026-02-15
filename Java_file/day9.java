eed603842bf08c58aaf678f059a83dc385b40156
Riegooo <christiandanielcagas0@gmail.com>
2026-02-15 10:52:01 +0800

    Day 9 - convert array to a comma separated string (array join)

---
+++ Java_file/day9.java
@@ -0,0 +1,12 @@
+package coding_10min_a_day.Java_file;
+
+public class day9 {
+    public static void main(String[] args) {
+        
+        String[] prog_language = {"Java","JavaScript", "C#"};
+
+        String show = String.join(", ", prog_language);
+        System.out.println(show);
+
+    }
+}
