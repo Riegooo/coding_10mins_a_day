2d93283ff2954441f1298d7a80e6f0e625ab6f64
Riegooo <christiandanielcagas0@gmail.com>
2026-02-17 13:41:20 +0800

    Day 11 - split string to an array of (single) characters

---
+++ Java_file/day11.java
@@ -0,0 +1,12 @@
+package coding_10min_a_day.Java_file;
+
+public class day11 {
+    public static void main(String[] args) {
+        String str = "Hello world";
+
+        char[] array_of_char = str.toCharArray();
+        for(char Character :array_of_char){
+            System.out.print(Character + " ");
+        };
+    }
+}
