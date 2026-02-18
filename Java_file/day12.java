46f3ea46933c0a59aac9fc18fc8f153771e98c6b
Riegooo <christiandanielcagas0@gmail.com>
2026-02-18 09:50:12 +0800

    Day 12 - loop through a numeric array preferrably not using manual index increment

---
+++ Java_file/day12.java
@@ -0,0 +1,12 @@
+package coding_10min_a_day.Java_file;
+
+public class day12 {
+    public static void main(String[] args) {
+        String[] prog_language = {"Java","JavaScript", "C#"};
+
+        for (String string : prog_language) {
+            System.out.println(string);
+        }
+
+    }
+}
