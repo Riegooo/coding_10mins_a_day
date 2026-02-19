c24619609617e62a06610e5de011f0db2c690cd8
Riegooo <christiandanielcagas0@gmail.com>
2026-02-19 20:51:49 +0800

    13 - loop through a numeric array printing both the index and the value

---
+++ Java_file/day13.java
@@ -0,0 +1,11 @@
+package coding_10min_a_day.Java_file;
+
+public class day13 {
+    public static void main(String[] args) {
+        
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for(int i = 0; i < nums.length; i++) {
+            System.out.println("INDEX: "+ i + " VALUE: " + nums[i]);
+        }
+    }
+}
